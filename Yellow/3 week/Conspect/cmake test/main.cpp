2d7bd2742c4d62b1dd9b0928e6bc9c4913ada231
Nikita <67433712+NikomSol@users.noreply.github.com>
2024-03-07 20:46:44 +0300

    cmake fiasko

---
+++ Yellow/3 week/Conspect/cmake test/main.cpp
@@ -0,0 +1,18 @@
+// https://www.youtube.com/watch?v=bkksn6KUugg
+// Ctrl+Shift+P
+// C++ Edin conf (UI)
+// C++ Add debug conf
+
+// CMake: Quick start
+
+// Fiasko
+// Unable to determine what CMake generator to use. 
+// Please install or configure a preferred generator, 
+// or update settings.json, your Kit configuration or PATH variable.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello";
+    return 0;
+}
